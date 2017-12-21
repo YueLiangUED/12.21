647a64cdd81d09b9e533a451739c960b1ebae3ea
wangbiaozy <56565283@qq.com>
2017-12-21 17:05:14 +0800

    1

--- js/index.js
+++ js/index.js
@@ -9,7 +9,29 @@
     global.addEventListener('resize',remChange,false);
 })(window);
 $(function () {
-    
+
+
+
+    //显示弹窗1
+    function showTc_1() {
+        $('.tc_1').show();
+        showMask();
+    }
+    //关闭弹窗1
+    function hideTc_1() {
+        $('.tc_1').hide();
+        hideMask();
+    }
+    //显示弹窗2
+    function showTc_2() {
+        $('.tc_2').show();
+        showMask();
+    }
+    //关闭弹窗2
+    function hideTc_2() {
+        $('.tc_2').hide();
+        hideMask();
+    }
     //显示遮罩层    
     function showMask(){
         $("#mask").css("height",$(document).height());
